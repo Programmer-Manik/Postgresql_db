3cc75f4a134765875491eef3edd1a36bc1613f90
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-08-08 20:40:55 +0600

    Expanding on the ALTER Keyword for Table Modification

--- alter.sql
+++ alter.sql
@@ -1,2 +1,25 @@
 -- Active: 1723122519319@@127.0.0.1@5432@ph
 SELECT * FROM person2;
+
+ALTER TABLE person2
+ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' not NULL;
+
+ALTER TABLE person2
+    DROP COLUMN email;
+
+
+ALTER TABLE person2
+ RENAME COLUMN age to user_age;
+
+
+ALTER TABLE person2
+  ALTER COLUMN user_name TYPE VARCHAR(18)
+
+ALTER TABLE person2
+  ALTER COLUMN user_age set NOT NULL
+
+ALTER TABLE person2
+  ALTER COLUMN user_age DROP NOT NULL
+
+INSERT INTO person2 VALUES(7, 'text', 45, 'manik@gmail.com');
+
