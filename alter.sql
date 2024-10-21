4f13a2fe0f2bfb8b19e45b639f3001bff8c8ff7f
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-10-22 02:36:28 +0600

    Different Methods to Alter Tables For Primary key, Unique etc

--- alter.sql
+++ alter.sql
@@ -13,23 +13,27 @@ VALUES
 ('Charlie Brown', 35),
 ('Emily Davis', 28);
 
-
+DROP TABLE semanik
 
 ALTER TABLE semanik 
     ADD COLUMN email  VARCHAR(20) DEFAULT 'manik@gmailcom' NOT NULL
+
+ALTER TABLE semanik 
+    ADD COLUMN age  INTEGER DEFAULT 32 NOT NULL
 
 
 INSERT INTO semanik VALUES( 6,'manik', 33, 'John@gmail.com')
 
 
 ALTER TABLE semanik 
-    DROP COLUMN email  ;
+    DROP COLUMN user_Age  ;
 
 ALTER TABLE semanik
     RENAME COLUMN age to user_age;
     
 ALTER TABLE semanik
-    ALTER COLUMN user_age TYPE VARCHAR(20)
+    ALTER COLUMN user_Age TYPE INTEGER;
+
 
 ALTER TABLE semanik
     ALTER COLUMN user_age set NOT NULL
@@ -37,6 +41,17 @@ ALTER TABLE semanik
 ALTER TABLE semanik
     ALTER COLUMN user_age DROP NOT NULL
 
+ALTER TABLE semanik
+   ADD constraint unique_person2_user_age UNIQUE(user_age);
+ALTER TABLE semanik
+   DROP constraint unique_person2_user_age ;
+
+ALTER TABLE semanik
+   ADD constraint pk_person2_user_age PRIMARY KEY(id);
+
+TRUNCATE TABLE semanik 
+
+INSERT INTO semanik VALUES(7, 'text', 45, 'manik@gmail.com');
 
 
 SELECT * FROM semanik
